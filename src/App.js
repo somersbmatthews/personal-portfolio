0f0065e39ec2432f58e3ddb1db0ec1602cc2dfbb
Somers Matthews <somersbmatthews@gmail.com>
2019-05-13 15:22:39 -0500

    sucessfully implemented Radium with react-animations with fadeIn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import Nav from './components/Nav'
+import React, {Component} from 'react';
+import Nav from './components/Nav';
 
+import {fadeIn} from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
 
+const styles = {
+  fadeIn: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeIn, 'fadeIn')
+  }
+}
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-          <div>About</div>
-          <div>Contact</div>
-          <div>Projects</div>          
-    </div>
-  );
+class App extends Component {
+    render() {
+      return (
+        <StyleRoot>
+          <div className='test' style={styles.fadeIn}>
+          <h1>Test of Radium with react-animations with fadeIn</h1>
+          </div>
+        </StyleRoot>
+      )
+    };
 }
 
 export default App;
+
+{/* <div className="App">
+<div>About</div>
+<div>Contact</div>
+<div>Projects</div>          
+</div> */}
